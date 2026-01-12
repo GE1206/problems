a30be448091b2c5275ce74611794ca190a0d4bb2
Kelvin Yelyen <52675327+kelvinyelyen@users.noreply.github.com>
2026-01-12 16:58:53 +0000

    Update __init__.py

--- pset1/__init__.py
+++ pset1/__init__.py
@@ -3,47 +3,33 @@ import check50.c
 
 @check50.check()
 def exists():
-    """temperature.c exists"""
     check50.exists("temperature.c")
 
 @check50.check(exists)
 def compiles():
-    """temperature.c compiles"""
-    # lcs50=False means we do not link the CS50 library
     check50.c.compile("temperature.c", lcs50=False)
 
 @check50.check(compiles)
-def test_freezing():
-    """converts 0 Celsius correctly"""
-    check50.run("./temperature")\
-        .stdin("0")\
-        .stdout("0.00 Celsius = 32.00 Fahrenheit")\
-        .stdout("0.00 Celsius = 273.15 Kelvin")\
-        .exit(0)
+def test_0():
+    """0.00 Celsius yields 32.00 Fahrenheit and 273.15 Kelvin"""
+    check50.run("./temperature").stdin("0").stdout("0.00 Celsius = 32.00 Fahrenheit").stdout("0.00 Celsius = 273.15 Kelvin").exit(0)
 
 @check50.check(compiles)
-def test_boiling():
-    """converts 100 Celsius correctly"""
-    check50.run("./temperature")\
-        .stdin("100")\
-        .stdout("100.00 Celsius = 212.00 Fahrenheit")\
-        .stdout("100.00 Celsius = 373.15 Kelvin")\
-        .exit(0)
+def test_100():
+    """100.00 Celsius yields 212.00 Fahrenheit and 373.15 Kelvin"""
+    check50.run("./temperature").stdin("100").stdout("100.00 Celsius = 212.00 Fahrenheit").stdout("100.00 Celsius = 373.15 Kelvin").exit(0)
 
 @check50.check(compiles)
-def test_negative():
-    """converts -40 Celsius correctly"""
-    check50.run("./temperature")\
-        .stdin("-40")\
-        .stdout("-40.00 Celsius = -40.00 Fahrenheit")\
-        .stdout("-40.00 Celsius = 233.15 Kelvin")\
-        .exit(0)
+def test_37():
+    """37.00 Celsius yields 98.60 Fahrenheit and 310.15 Kelvin"""
+    check50.run("./temperature").stdin("37").stdout("37.00 Celsius = 98.60 Fahrenheit").stdout("37.00 Celsius = 310.15 Kelvin").exit(0)
 
 @check50.check(compiles)
-def test_body_temp():
-    """converts 37 Celsius (body temp) correctly"""
-    check50.run("./temperature")\
-        .stdin("37")\
-        .stdout("37.00 Celsius = 98.60 Fahrenheit")\
-        .stdout("37.00 Celsius = 310.15 Kelvin")\
-        .exit(0)
+def test_neg40():
+    """-40.00 Celsius yields -40.00 Fahrenheit and 233.15 Kelvin"""
+    check50.run("./temperature").stdin("-40").stdout("-40.00 Celsius = -40.00 Fahrenheit").stdout("-40.00 Celsius = 233.15 Kelvin").exit(0)
+
+@check50.check(compiles)
+def style():
+    """temperature.c follows style50"""
+    check50.run("style50 temperature.c").exit(0)
